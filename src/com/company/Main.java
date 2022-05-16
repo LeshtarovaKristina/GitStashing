faff41c5a3262348d5e00fdd18a582de09b4b4c6
LeshtarovaKristina <leshtarova1993@gmail.com>
2022-05-16 13:49:40 -0400

    The red Auddii car is ready with some basic features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,17 @@
 package com.company;
 
+
 public class Main {
+
     public static void main(String[] args) {
-        //write your code here
-        basicFeatures car = new basicFeatures();
-        
+        // write your code here
+        basicFeatures Auddi =new basicFeatures();
+        Auddi.Color="red";
+        Auddi.doors="4";
+        Auddi.engineCapac="V8";
+        Auddi.headlights="blue";
+        Auddi.regNo="tuzik";
+
+
     }
 }
